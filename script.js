502858eda1bdac69ab074d16216e97480ce1fd45
Léo Filoche <leofiloche.wk@gmail.com>
2023-04-22 16:52:42 +0200

    avoiding duplicates if typing too fast

--- script.js
+++ script.js
@@ -5,38 +5,44 @@ let visible_images;
 
 const max_results = 50;
 
-search.setAttribute('placeholder','joy...')
+search.setAttribute('placeholder', 'joy...')
 
 async function fetchImageFiles() {
   const response = await fetch("emoji-names.json");
-    return await response.json();
+  return await response.json();
 }
 
 fetchImageFiles().then(data => {
-    image_files = data;
-    visible_images = image_files.slice(0,max_results);
-    showVisible()
+  image_files = data;
+  visible_images = image_files.slice(0, max_results);
+  showFilteredImages()
 })
 
 
+// datetime of the latest time that showFilteredImages has been called
+let latestDisplayCall = 0; 
 
-async function showVisible(){
-  grid.innerHTML='';
-  for(let img of visible_images){
+async function showFilteredImages() {
+  const uniqueId = Date.now();
+  latestDisplayCall = uniqueId;
+  grid.innerHTML = '';
+  for (let img of visible_images) {
     let src = img['src']
-    await fetch(src).then(data=>{
-      let tag='<img src="'+data.url+'" title="'+img['name']+'"/>';
-      grid.innerHTML+=tag;
+    await fetch(src).then(data => {
+      grid.innerHTML += '<img src="' + data.url + '" title="' + img['name'] + '"/>';
     })
+    if (uniqueId !== latestDisplayCall) {
+      return;
+    }
   }
 }
 
-search.addEventListener('input',async e=>{
-    str = search.value;
-    visible_images = image_files.filter(image => {
-        const title = image['name']
-        return title.toLowerCase().includes(str.toLowerCase());
-      }).slice(0,max_results); 
-    await showVisible();
+search.addEventListener('input', async e => {
+  str = search.value;
+  visible_images = image_files.filter(image => {
+    const title = image['name']
+    return title.toLowerCase().includes(str.toLowerCase());
+  }).slice(0, max_results);
+  await showFilteredImages();
 })
 
